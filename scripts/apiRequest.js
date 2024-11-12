2f0b211ca6847facd3b6464504f59031d1b0b193
miguelangarano <miguelangarano@gmail.com>
2024-11-11 22:37:38 -0500

    try

--- scripts/apiRequest.js
+++ scripts/apiRequest.js
@@ -1,38 +1,42 @@
-const fs = require('fs');
-const https = require('https');
+const fs = require("fs");
+const https = require("https");
 
 // Encode the projectId and ciBuildId to ensure any special characters are URL-safe
 const projectId = encodeURIComponent(process.env.CURRENTS_PROJECT_ID);
 const ciBuildId = encodeURIComponent(process.env.CI_BUILD_ID);
 
-console.log("PROJ::", projectId, ciBuildId)
+console.log("PROJ::", projectId, ciBuildId);
 
 const options = {
-    hostname: 'api.currents.dev',
-    path: `/v1/runs/previous?projectId=${projectId}&ciBuildId=${ciBuildId}&pwLastRun=true`,
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${process.env.CURRENTS_API_KEY}`,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+  hostname: "api.currents.dev",
+  path: `/v1/runs/previous?projectId=${projectId}&ciBuildId=${ciBuildId}&pwLastRun=true`,
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${process.env.CURRENTS_API_KEY}`,
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 };
 
-const req = https.request(options, res => {
-    let data = '';
+const req = https.request(options, (res) => {
+  let data = "";
 
-    res.on('data', chunk => {
-        data += chunk;
-    });
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
 
-    res.on('end', () => {
-        fs.writeFileSync('.last-run.json', data);
-        console.log('Response saved to .last-run.json', data);
-    });
+  res.on("end", () => {
+    try {
+      fs.writeFileSync("scripts/.last-run.json", data);
+      console.log("Response saved to .last-run.json", data);
+    } catch (e) {
+      console.log("ERR::", e);
+    }
+  });
 });
 
-req.on('error', error => {
-    console.error(error);
+req.on("error", (error) => {
+  console.error(error);
 });
 
 req.end();
