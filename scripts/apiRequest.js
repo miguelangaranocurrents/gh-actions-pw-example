e73fad49e6dffac760f636c17687f6bc2f150023
miguelangarano <miguelangarano@gmail.com>
2024-11-11 23:02:06 -0500

    stringify

--- scripts/apiRequest.js
+++ scripts/apiRequest.js
@@ -29,7 +29,7 @@ const req = https.request(options, (res) => {
 
   res.on("end", () => {
     try {
-      fs.writeFileSync("scripts/.last-run.json", JSON.parse(data).pwLastRun);
+      fs.writeFileSync("scripts/.last-run.json", JSON.stringify(JSON.parse(data).pwLastRun));
       console.log("Response saved to .last-run.json", JSON.parse(data).pwLastRun);
     } catch (e) {
       console.log("ERR::", e);
