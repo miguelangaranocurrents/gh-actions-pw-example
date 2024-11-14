836c2cc2c9f517297bd9e53a1255fbad911c8042
miguelangarano <miguelangarano@gmail.com>
2024-11-14 17:51:00 -0500

    add more tests

---
+++ tests/feature.onboarding8.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  expect(true).toBe(true)
+});
+
+test('get started link', async ({ page }) => {
+  expect(true).toBe(true)
+});
