154e3393059a8d42c1830918b8e7a28b66b6345b
miguelangarano <miguelangarano@gmail.com>
2024-11-14 17:17:02 -0500

    test runner name

--- tests/feature.onboarding.spec.ts
+++ tests/feature.onboarding.spec.ts
@@ -1,32 +1,9 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test.describe(() => {
-  test("has title", async ({ page }, testInfo) => {
-    await page.goto("https://playwright.dev/");
+test('has title', async ({ page }) => {
+  expect(true).toBe(false)
+});
 
-    // Expect a title "to contain" a substring.
-    const screenshot = await page.screenshot();
-    await testInfo.attach("screenshot", {
-      body: screenshot,
-      contentType: "image/png",
-    });
-    const screenshot1 = await page.screenshot();
-    await testInfo.attach("screenshot1", {
-      body: screenshot,
-      contentType: "image/png",
-    });
-    await expect(page).toHaveTitle(/Cypress/);
-  });
-
-  test("get started link", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
-
-    // Click the get started link.
-    await page.getByRole("link", { name: "Get started" }).click();
-
-    // Expects page to have a heading with the name of Installation.
-    await expect(
-      page.getByRole("heading", { name: "Installation" })
-    ).toBeVisible();
-  });
+test('get started link', async ({ page }) => {
+  expect(true).toBe(true)
 });
